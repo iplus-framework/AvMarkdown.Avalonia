729c21059560c872131cb5c3c126e24fbf04fa14
whistyun <kanadische_goldrute@yahoo.co.jp>
2026-04-09 01:35:29 +0900

    Simply migration; refs #179

--- Markdown.Avalonia.Tight/Extensions/DivideColorExtension.cs
+++ Markdown.Avalonia.Tight/Extensions/DivideColorExtension.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Data;
 using Avalonia.Data.Converters;
+using Avalonia.Data.Core;
 using Avalonia.Markup.Xaml;
 using Avalonia.Markup.Xaml.MarkupExtensions;
 using Avalonia.Media;
@@ -27,10 +28,10 @@ namespace Markdown.Avalonia.Extensions
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            IBinding left;
+            BindingBase left;
             if (Color.TryParse(_frmKey, out var leftColor))
             {
-                left = new StaticBinding(leftColor);
+                left = CompiledBinding.Create<Color, Color>(v=>v, leftColor);
             }
             else
             {
@@ -38,10 +39,10 @@ namespace Markdown.Avalonia.Extensions
                 left = lftExt.ProvideValue(serviceProvider);
             }
 
-            IBinding right;
+            BindingBase right;
             if (Color.TryParse(_toKey, out var rightColor))
             {
-                right = new StaticBinding(rightColor);
+                right = CompiledBinding.Create<Color, Color>(v=>v, rightColor);
             }
             else
             {
@@ -51,96 +52,10 @@ namespace Markdown.Avalonia.Extensions
 
             return new MultiBinding()
             {
-                Bindings = new IBinding[] { left, right },
+                Bindings = new BindingBase[] { left, right },
                 Converter = new DivideConverter(_relate)
             };
         }
-    }
-
-    class StaticBinding : IBinding
-    {
-        private readonly object _value;
-
-        public StaticBinding(object value)
-        {
-            _value = value;
-        }
-
-        public InstancedBinding? Initiate(AvaloniaObject target, AvaloniaProperty? targetProperty, object? anchor = null, bool enableDataValidation = false)
-        {
-            return InstancedBinding.OneWay(new StaticBindingObservable(_value));
-        }
-
-        class StaticBindingObservable : IObservable<object>
-        {
-            object Value { get; set; }
-
-            public StaticBindingObservable(object value)
-            {
-                Value = value;
-            }
-
-            private readonly ConcurrentDictionary<StaticTicket, IObserver<object>> _cache = new();
-
-            public IDisposable Subscribe(IObserver<object> observer)
-            {
-                //StaticTicket ticket;
-                //do
-                //{
-                //    ticket = new StaticTicket(this);
-                //} while (Cache.ContainsKey(ticket));
-                //
-                //Cache[ticket] = observer;
-                //
-                //return ticket;
-
-                observer.OnNext(Value);
-
-                return new DummyDisposable();
-            }
-
-            public void Remove(StaticTicket nemui)
-            {
-                _cache.TryRemove(nemui, out var _);
-            }
-        }
-
-        class DummyDisposable : IDisposable
-        {
-            public void Dispose() { }
-        }
-
-        class StaticTicket : IDisposable
-        {
-            private readonly StaticBindingObservable _owner;
-            private readonly Guid _guid = new();
-
-            public StaticTicket(StaticBindingObservable owner)
-            {
-                this._owner = owner;
-            }
-
-
-            ~StaticTicket() => Dispose();
-
-            public override int GetHashCode()
-                => _guid.GetHashCode();
-
-            public override bool Equals(object? obj)
-            {
-                if (obj is StaticTicket nemu)
-                    return nemu._guid.Equals(nemu._guid);
-
-                return false;
-            }
-
-            public void Dispose()
-            {
-                _owner.Remove(this);
-                GC.SuppressFinalize(this);
-            }
-        }
-
     }
 
     class DivideConverter : IMultiValueConverter
